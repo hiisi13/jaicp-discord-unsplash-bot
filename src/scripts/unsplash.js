9b5c201b0b5d1de145d717aeebfc22ae572117c8
Dmitry Kozhedubov <hiisi13@gmail.com>
2022-01-27 17:15:14 +0300

    Update unsplash.js

--- src/scripts/unsplash.js
+++ src/scripts/unsplash.js
@@ -10,19 +10,6 @@ var UnsplashAPI = {
             },
             timeout: 10000
         });
-        log(pictures.data);
         return pictures.data;
-    },
-    
-    random: function () {
-        var picture = $http.get("https://api.unsplash.com/photos/random", {
-            dataType: "json",
-            headers: {
-                "Authorization": "Client-ID LLbR7h_Ce0ntXaTwR8T31Tz8hArUB2oLJgi94JSnNaM"
-            },
-            timeout: 10000
-        });
-        log(picture.data);
-        return picture.data;    
     }
 };
